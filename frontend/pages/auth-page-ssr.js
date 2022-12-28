6eb6a428a90e1942db0407530919d5cf029cb783
scher <luishenriqueost@gmail.com>
2022-12-28 12:19:11 -0300

    Comentários da aula 2

--- frontend/pages/auth-page-ssr.js
+++ frontend/pages/auth-page-ssr.js
@@ -28,3 +28,27 @@ export async function getServerSideProps(ctx) {
   }
 }
 
+/*
+==== COMENTÁRIOS DA AULA ====
+- Como esta página usa informações que precisam estar disponíveis e atualizadas no momento da renderização
+é usado getServerSideProps.
+
+- getServerSideProps é uma função que vai ser executada toda vez que a página for requisitada e seu retorno
+será automaticamente fornecido para o componente da página como props. Primeiro vai executar o que estiver
+em getServerSideProps e depois vai montar a página.
+
+"Note that irrespective of rendering type, any props will be passed to the page component and can be viewed 
+on the client-side in the initial HTML. This is to allow the page to be hydrated correctly. Make sure that 
+you don't pass any sensitive information that shouldn't be available on the client in props."
+
+A varíavel padrão de entrada para o getServerSideProps é o contexto da página a ser gerado, nele vão constar
+os cookies por exemplo.
+
+getServerSideProps só pode ser utilizado em arquivos que renderizam páginas, não pode ser utilizado em outros
+tipos de arquivos.
+
+- Neste caso a gente está printando todas as props geradas pelo getServerSideProps.
+
+- Para buscar o token precisamos passar o contexto (ctx) da página para o tokenService.
+
+*/
